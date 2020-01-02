5afa3ba8bdaac3f0a31393f7ca34a0607139ab73
KevinPR1 <kevinpreira.p@gmail.com>
2020-01-02 17:44:57 +0100

    CleanupDatabase completed

--- todoc-master/app/src/main/java/com/cleanup/todoc/database/CleanupDatabase.java
+++ todoc-master/app/src/main/java/com/cleanup/todoc/database/CleanupDatabase.java
@@ -1,9 +1,61 @@
 package com.cleanup.todoc.database;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.ContentValues;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.cleanup.todoc.database.dao.ProjectDao;
+import com.cleanup.todoc.database.dao.TaskDao;
+
 /**
  * Created by Kevin  - Openclassrooms on 02/01/2020
  */
-public class CleanupDatabase {
+public abstract class CleanupDatabase extends RoomDatabase {
 
+    // --- SINGLETON ---
+    private static volatile CleanupDatabase INSTANCE;
+
+    // --- DAO ---
+    public abstract TaskDao taskDao();
+
+    public abstract ProjectDao projectDao();
+
+
+    // --- INSTANCE ---
+    public static CleanupDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (CleanupDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            CleanupDatabase.class, "MyDatabase.db")
+                            .addCallback(prepopulateDatabase())
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+
+    private static Callback prepopulateDatabase() {
+        return new Callback() {
+
+            @Override
+            public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                super.onCreate(db);
+
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("id", 1);
+                contentValues.put("task", "Salle 2");
+
+                db.insert("Project", OnConflictStrategy.IGNORE, contentValues);
+            }
+        };
+
+    }
 
 }
